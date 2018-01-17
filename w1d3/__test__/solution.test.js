42707704acb92eab66f4210093d56f071390a853
Mitchell M <mitchellm1618@gmail.com>
2018-01-17 14:55:49 -0800

    first test case passing, solution.js code functioning as intended

--- w1d3/__test__/solution.test.js
+++ w1d3/__test__/solution.test.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const solution = require('../lib/solution');
+
+describe('this is a test to determine whether our function is able to traverse the desired object literals, and produce the desired output ', function(){
+  const car4 = {
+    passengers: 1,
+    next: null
+  };
+  const car3 = {
+    passengers: 2,
+    next: car4
+  };
+  const car2 = {
+    passengers: 6,
+    next: car3
+  };
+  const car1 = {
+    passengers: 2,
+    next: car2
+  };
+  const engine = {
+    passengers: 2,
+    next: car1
+  };
+  test('determining whether function is able to calculate total passengers on train from each respective car', function() {
+    expect(solution.traverse(engine)).toEqual(13);
+  })
+})
--- w1d3/lib/solution.js
+++ w1d3/lib/solution.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const solution = module.exports = {};
+
+solution.traverse = (car) => {
+  let total = 0;
+  while (car.next !== null) {
+    console.log('passengers: ', car.passengers);
+    total += car.passengers;
+    car = car.next;
+  }
+  console.log('final car passengers: ', car.passengers);
+  return total + car.passengers;
+};
