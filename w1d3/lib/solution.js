8dbb19214e1d2ea6745a7d3351bcd732f6c285b0
Mitchell M <mitchellm1618@gmail.com>
2018-01-17 15:15:40 -0800

    modified solution to catch edge cases, completed and passing two more tests

--- w1d3/lib/solution.js
+++ w1d3/lib/solution.js
@@ -3,12 +3,14 @@
 const solution = module.exports = {};
 
 solution.traverse = (car) => {
+  if (typeof (car) !== 'object') return 'error, incorrect data type';
   let total = 0;
   while (car.next !== null) {
+    if (!car.passengers || !car.next) return `current total: ${total}, data entry error`;
     console.log('passengers: ', car.passengers);
     total += car.passengers;
     car = car.next;
   }
   console.log('final car passengers: ', car.passengers);
-  return total + car.passengers;
+  return `total passengers: ${total + car.passengers}`;
 };
