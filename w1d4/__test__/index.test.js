53898d7cc8e18e29d7f7c3becbb04f88b044f137
Mitchell M <mitchellm1618@gmail.com>
2018-01-18 13:54:21 -0800

    added working function, npm packages

--- w1d4/__test__/index.test.js
+++ w1d4/__test__/index.test.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const index = require('../index');
+
+const first = ['ajax', 'bunny', 'berenger', 'carrot'];
+const second = ['apple', 'bunny', 'bundy', 'carrot', 'kiwi', 'red'];
+
+intersect(first, second);
--- w1d4/lib/index.js
+++ w1d4/lib/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const index = module.exports = {};
+
+index.intersect = function (arr1, arr2) {
+  let duplicate = [];
+  for (let i in arr1) {
+    if (arr2.includes(arr1[i])) {
+      duplicate.push(arr1[i]);
+    }
+  }
+  return duplicate;
+};
