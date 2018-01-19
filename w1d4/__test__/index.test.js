2240aa9a3d26f02f0cf944fdfd2789b53ea7b49a
Mitchell M <mitchellm1618@gmail.com>
2018-01-18 19:51:42 -0800

    tests completed, modified index.js function as needed

--- w1d4/__test__/index.test.js
+++ w1d4/__test__/index.test.js
@@ -1,8 +1,26 @@
 'use strict';
 
-const index = require('../index');
+const index = require('../lib/index');
 
-const first = ['ajax', 'bunny', 'berenger', 'carrot'];
-const second = ['apple', 'bunny', 'bundy', 'carrot', 'kiwi', 'red'];
+describe('index', function() {
+  it('should return array with duplicates with valid data entry', function(done) {
+    const first = ['ajax', 'bunny', 'berenger', 'carrot'];
+    const second = ['apple', 'bunny', 'bundy', 'carrot', 'kiwi', 'red'];
+    expect(index.intersect(first, second)).toEqual(['bunny', 'carrot']);
+    done();
+  });
 
-intersect(first, second);
+  it('should return array with duplicates with valid data entry, without multiple entries if data set contains multiple instances', function(done) {
+    const first = ['ajax', 'bunny', 'bunny', 'bunny', 'berenger', 'carrot', 'carrot'];
+    const second = ['apple', 'bunny', 'bundy', 'carrot', 'kiwi', 'red'];
+    expect(index.intersect(first, second)).toEqual(['bunny', 'carrot']);
+    done();
+  });
+
+  it('should return a data entry error with either array being empty', function(done) {
+    const first = ['ajax', 'bunny', 'berenger', 'carrot'];
+    const second = [];
+    expect(index.intersect(first, second)).toEqual('DATA ENTRY ERROR');
+    done();
+  });
+});
--- w1d4/lib/index.js
+++ w1d4/lib/index.js
@@ -3,9 +3,12 @@
 const index = module.exports = {};
 
 index.intersect = function (arr1, arr2) {
+  if (arr1.length === 0 || arr2.length === 0) {
+    return 'DATA ENTRY ERROR';
+  }
   let duplicate = [];
   for (let i in arr1) {
-    if (arr2.includes(arr1[i])) {
+    if (arr2.includes(arr1[i]) && !duplicate.includes(arr1[i])) {
       duplicate.push(arr1[i]);
     }
   }
