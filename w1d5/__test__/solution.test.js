7775d3f19ed0f421dca13e6ab1d2e8e3c2b6f4ce
Mitchell M <mitchellm1618@gmail.com>
2018-01-19 15:08:47 -0800

    first test passing

--- w1d5/__test__/solution.test.js
+++ w1d5/__test__/solution.test.js
@@ -1,22 +1,22 @@
 'use strict';
 
-const solution = require('./solution');
+const solution = require('../lib/solution');
 
 describe('w1d5 whiteboarding', function () {
   it('should reuturn the middle index object ', function (done) {
     const sLL = {
       value: 25,
       next: {
-        value: 01,
+        value: 1,
         next: {
           value: 15,
           next: {
             value: 33,
             next: {
-              value: 04,
+              value: 4,
               next: null
             }}}}};
-    expect(solution.middleNode(sLL)).toBe()
+    expect(solution.middleNode(sLL)).toEqual([15, { value: 33, next: { value: 4, next: null } }])
     done();
   });
 });
