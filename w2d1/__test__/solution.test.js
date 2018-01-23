2993f7c352efc1493715797c30f52d4ac616f9a2
Mitchell M <mitchellm1618@gmail.com>
2018-01-22 16:04:52 -0800

    deleted repo, weird errors

--- w2d1/__test__/solution.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const solution = require('../lib/solution');
-
-describe('w1d5 whiteboarding', function () {
-  it('', function(done) {
-
-  };
-});  
--- w2d1/lib/solution.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const solution = module.exports = {};
-
-solution.
