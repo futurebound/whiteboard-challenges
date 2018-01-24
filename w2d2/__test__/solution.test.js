05ab1e85308339bfa6de73e22b6e14fdfb35f1cb
Mitchell M <mitchellm1618@gmail.com>
2018-01-23 21:56:47 -0800

    created scaffold

---
+++ w2d2/__test__/solution.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const solution = require('../lib/solution');
+
+describe('solution.js', function() {
+  it('', function(done) {
+    expect();
+    done();
+  });
+
+  it('', function(done) {
+    expect();
+    done();
+  });
+
+  it('', function(done) {
+    expect();
+    done();
+  });
+});
