30f69ccad8316f216949cd755b6503a2b57bbebc
Mitchell M <mitchellm1618@gmail.com>
2018-01-24 21:37:22 -0800

    added function to solution.js

--- w2d3/lib/solution.js
+++ w2d3/lib/solution.js
@@ -2,4 +2,15 @@
 
 const solution = module.exports = {};
 
-solution.duplicate
+solution.duplicateNodes = function(list1, list2) {
+  let arr1 = [], arr2 = [], duplicates = [];
+  while(list1.next) {
+    if(typeof(arr1.value) === 'number') arr1.push(list1.value);
+  }
+  while(list2.next) {
+    if(typeof(arr2.vale) === 'number') arr2.push(list2.val);
+  }
+  for (let i in arr1) {
+    if (arr2.includes(arr1[i])) duplicates.push(arr1[i]);
+  }
+};
