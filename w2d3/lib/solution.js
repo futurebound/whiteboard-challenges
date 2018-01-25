6d3e6e6940d0d500e4707c13f01803b231d09f2d
Mitchell M <mitchellm1618@gmail.com>
2018-01-24 21:42:11 -0800

    completed solution.js

--- w2d3/lib/solution.js
+++ w2d3/lib/solution.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const Nd = require('./nd');
+const SLL = require('./sll');
+
 const solution = module.exports = {};
 
 solution.duplicateNodes = function(list1, list2) {
@@ -13,4 +16,12 @@ solution.duplicateNodes = function(list1, list2) {
   for (let i in arr1) {
     if (arr2.includes(arr1[i])) duplicates.push(arr1[i]);
   }
+
+  let duplicateSLL = new SLL();
+
+  for(let i in duplicates) {
+    duplicateSLL.insertEnd(duplicates[i]);
+  }
+
+  return duplicateSLL;
 };
