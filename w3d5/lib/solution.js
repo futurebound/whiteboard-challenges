647f2aa4993a5a7e45b58e144912b4a50d0e8727
Mitchell M <mitchellm1618@gmail.com>
2018-02-02 13:43:09 -0800

    created scaffold, added pseudo-image

---
+++ w3d5/lib/solution.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Kary = require('./kary-tree');
+const Queue = require('./queue');
+
+const solution = module.exports =  {};
+
+solution.childlessNodes = function(root) {
+
+}
