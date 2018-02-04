e3e79af44875108e0dc2a89f628a317b98894ef6
Mitchell M <mitchellm1618@gmail.com>
2018-02-04 12:08:56 -0800

    all tests passing for solution2

--- w2d5/lib/solution2.js
+++ w2d5/lib/solution2.js
@@ -5,7 +5,27 @@ const solution = module.exports = {};
 solution.binarySearch = function(n, arr) {
   if(!n || !arr) return new Error('please pass both a number and an array');
   if(typeof n !== 'number') return new Error('n must be a number');
+  
+  let midpoint = Math.floor(arr.length / 2), bottom = 0, top = arr.length -1;
+  
   if(arr instanceof Array) {
-    let wer = wer;
+    if(!arr.length) return new Error('empty array');
+    if(!arr.includes(n)) return new Error('n not in array');
+    recurse();
+    return {value: n, index: midpoint};
   } else return new Error('please pass an array');
+
+  function recurse() {
+    if (arr[midpoint] === n) {
+      return;
+    } else if (n < arr[midpoint]) {
+      top = midpoint;
+      midpoint = Math.floor((top + bottom) / 2);
+      recurse();
+    } else if (n > arr[midpoint]) {
+      bottom = midpoint;
+      midpoint = Math.ceil((bottom + top) / 2);
+      recurse();
+    }
+  }
 };
