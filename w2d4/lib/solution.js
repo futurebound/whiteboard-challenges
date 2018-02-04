e252d1d2cf0aaaea465d626608441b31a0558ed5
Mitchell M <mitchellm1618@gmail.com>
2018-02-03 19:00:00 -0800

    linter running without errors

--- w2d4/lib/solution.js
+++ w2d4/lib/solution.js
@@ -3,7 +3,7 @@
 const solution = module.exports = {};
 
 solution.nthFromLast = function(n, sll) {
-  if(!n || !sll) return new Error('please enter two arguments: a target number, and a singly linked list')
+  if(!n || !sll) return new Error('please enter two arguments: a target number, and a singly linked list');
   if(typeof n !== 'number') return new Error('please enter a number to target');
   if(!sll.head) return new Error('empty list');
   if(n > sll.length) return new Error('target not in list');
