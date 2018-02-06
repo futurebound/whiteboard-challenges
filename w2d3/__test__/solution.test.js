6b35003f601440aa3b040dfc7038b1967624725d
Mitchell M <mitchellm1618@gmail.com>
2018-02-06 08:31:09 -0800

    refactoring solution, likely have to refactor SLL constructor too

--- w2d3/__test__/solution.test.js
+++ w2d3/__test__/solution.test.js
@@ -1,21 +1,31 @@
 'use strict';
 
-const Nd = require('./nd');
-const SLL = require('./sll');
+const SLL = require('../lib/sll');
 
 const solution = require('../lib/solution');
+require('jest');
 
 
-describe('', function () {
+describe('#solution', function () {
+  beforeAll(() => {
+    this.sll1 = new SLL().insertEnd(1).insertEnd(3).insertEnd(5);
+    this.sll2 = new SLL().insertEnd(1).insertEnd(2).insertEnd(5);
+    console.log('sll1:', this.sll1);
+    console.log('sll2:', this.sll2);
+  });
+
   describe(`valid input/output`, () => {
-    it(‘’, () => {
-      expect().;
+    it('should return an SLL', () => {
+      expect(solution.duplicateValues(this.sll1.head, this.sll2.head)).toBeInstanceOf(SLL);
+    });
+    it('should return an SLL with head value', () => {
+      expect(solution.duplicateValues(this.sll1.head, this.sll2.head)).not.toBeNull();
     });
   });
 
-  describe(`invalid input/output`, () => {
-    it(‘’, () => {
-      expect().;
-    });
-  });
+  // describe(`invalid input/output`, () => {
+  //   it('', () => {
+  //     expect()
+  //   });
+  // });
 });
--- w2d3/lib/solution.js
+++ w2d3/lib/solution.js
@@ -1,20 +1,21 @@
 'use strict';
 
-const Nd = require('./nd');
 const SLL = require('./sll');
 
 const solution = module.exports = {};
 
-solution.duplicateNodes = function(list1, list2) {
+solution.duplicateValues = function(list1, list2) {
   let arr1 = [], arr2 = [], duplicates = [];
   let duplicateSLL = new SLL();
 
   while(list1.next) { //loop pushing all node values into array
     if(typeof(arr1.value) === 'number') arr1.push(list1.value);
+    list1 = list1.next;
   }
 
   while (list2.next) { //loop pushing all node values into array
     if(typeof(arr2.vale) === 'number') arr2.push(list2.val);
+    list2 = list2.next;
   }
 
   for (let i in arr1) { //loop checking first array for duplicates present in second array, and that duplicates array doesn't already include value
@@ -25,7 +26,8 @@ solution.duplicateNodes = function(list1, list2) {
   for(let i in duplicates) {
     duplicateSLL.insertEnd(duplicates[i]);
   }
-
+  console.log('duplicates:', duplicates);
+  console.log('duplicateSLL:', duplicateSLL);
   //returns duplicate SLL as output
   return duplicateSLL;
 };
