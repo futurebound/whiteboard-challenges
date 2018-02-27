183806c4375c43e4de583833d03c6d63fa16bb2e
Mitchell M <mitchellm1618@gmail.com>
2018-02-27 15:02:24 -0800

    added iterative tests, passing, and fixed linter errors

--- w7d2/__test__/solution.test.js
+++ w7d2/__test__/solution.test.js
@@ -5,6 +5,9 @@ const solution = require('../lib/solution');
 describe('#solution', function() {
   describe('#iterative', function() {
     describe('valid input/output', () => {
+      it('should return a number as output with valid input', () => {
+        expect(typeof solution.recursive(4)).toEqual('number');
+      });
       it('should return value of 3 for an input of 4', () => {
         expect(solution.iterative(4)).toEqual(3);
       });
@@ -14,7 +17,30 @@ describe('#solution', function() {
     });
     
     describe('invalid input/output', () => {
-    
+      it('should return an error if not passed an argument', () => {
+        expect(solution.recursive()).toBeInstanceOf(Error);
+      });
+      it('should return an error message if not passed an argument', () => {
+        expect(solution.recursive().message).toMatch(/please pass/);
+      });
+      it('should return an error if not passed a number', () => {
+        expect(solution.recursive('three')).toBeInstanceOf(Error);
+      });
+      it('should return an error message if not passed a number', () => {
+        expect(solution.recursive('three').message).toMatch(/please pass/);
+      });
+      it('should return an error if passed a number less than 0', () => {
+        expect(solution.recursive(-5)).toBeInstanceOf(Error);
+      });
+      it('should return an error message if passed a number less than 0', () => {
+        expect(solution.recursive(-5).message).toMatch(/please pass/);
+      });
+      it('should return an error if not passed an integer', () => {
+        expect(solution.recursive(3.76)).toBeInstanceOf(Error);
+      });
+      it('should return an error message if not passed an integer', () => {
+        expect(solution.recursive(3.76).message).toMatch(/please pass/);
+      });
     });
   });
 
@@ -22,7 +48,7 @@ describe('#solution', function() {
     describe('valid input/output', () => {
       it('should return a number as output with valid input', () => {
         expect(typeof solution.recursive(4)).toEqual('number');
-      })
+      });
       it('should return value 3 at index 4 of fibonacci series', () => {
         expect(solution.recursive(4)).toEqual(3);
       });
@@ -34,25 +60,25 @@ describe('#solution', function() {
     describe('invalid input/output', () => {
       it('should return an error if not passed an argument', () => {
         expect(solution.recursive()).toBeInstanceOf(Error);
-      })
+      });
       it('should return an error message if not passed an argument', () => {
         expect(solution.recursive().message).toMatch(/please pass/);
       });
       it('should return an error if not passed a number', () => {
         expect(solution.recursive('three')).toBeInstanceOf(Error);
-      })
+      });
       it('should return an error message if not passed a number', () => {
         expect(solution.recursive('three').message).toMatch(/please pass/);
       });
       it('should return an error if passed a number less than 0', () => {
         expect(solution.recursive(-5)).toBeInstanceOf(Error);
-      })
+      });
       it('should return an error message if passed a number less than 0', () => {
         expect(solution.recursive(-5).message).toMatch(/please pass/);
       });
       it('should return an error if not passed an integer', () => {
         expect(solution.recursive(3.76)).toBeInstanceOf(Error);
-      })
+      });
       it('should return an error message if not passed an integer', () => {
         expect(solution.recursive(3.76).message).toMatch(/please pass/);
       });
